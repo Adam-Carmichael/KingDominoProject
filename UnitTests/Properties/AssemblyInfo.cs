4e59f4f9a581784e056b2e19bdc05335e4c16b6a
bjm31 <bjm31@students.uwf.edu>
2018-04-24 18:53:20 -0500

    added dependencies and references

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90343035-d73b-4657-8f43-e9c4b14d5084")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingDomino;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ViewModelUnitTests : ViewModel
+    {
+        Domino ChosenDomino = new Domino();
+
+        [TestMethod]
+        public void UpdateChosenDominoTest()
+        {
+            ChosenDomino = CurrentDominos[index];
+            OnPropertyChanged("ChosenDomino");
+            ShowChosenButtons = Visibility.Visible;
+            OnPropertyChanged("ShowChosenButtons");
+            ShowButtons = Visibility.Hidden;
+            OnPropertyChanged("ShowButtons");
+        }
+    }
+}
