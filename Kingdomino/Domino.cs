33e321bb7d11eafacdf9ce69c5d4b4de7a39ace2
Bryan Mort <32080274+bjm31@users.noreply.github.com>
2018-04-24 16:37:37 -0500

    Fix

--- Kingdomino/Domino.cs
+++ Kingdomino/Domino.cs
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-﻿using System;
-=======
 using System;
->>>>>>> UI-Development2
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-=======
 using System.Windows.Media.Imaging;
->>>>>>> UI-Development2
 
 namespace KingDomino
 {
@@ -28,24 +21,12 @@ namespace KingDomino
             get { return tile2; }
             set { tile2 = value; }
         }
-<<<<<<< HEAD
-        private String dominoBack;
-        public String DominoBack
-=======
         private BitmapImage dominoBack;
         public BitmapImage DominoBack
->>>>>>> UI-Development2
         {
             get { return dominoBack; }
             set { dominoBack = value; }
         }
-<<<<<<< HEAD
-        public Domino(Tile tile1, Tile tile2, String dominoBack)
-        {
-            this.tile1 = tile1;
-            this.tile2 = tile2;
-            this.dominoBack = dominoBack;
-=======
 
         private int number;
         public int Number
@@ -59,7 +40,6 @@ namespace KingDomino
             this.tile2 = tile2;
             this.dominoBack = new BitmapImage(new Uri(dominoBack, UriKind.Relative));
             this.number = number;
->>>>>>> UI-Development2
         }
     }
 }
