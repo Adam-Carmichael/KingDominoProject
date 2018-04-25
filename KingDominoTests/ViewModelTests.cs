2667ecd9c5bea0e33444c1396089bfecf38c71ee
bjm31 <bjm31@students.uwf.edu>
2018-04-25 12:11:00 -0500

    added ViewModelTest methodes

--- KingDominoTests/ViewModelTests.cs
+++ KingDominoTests/ViewModelTests.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingDomino;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KingDominoTests
+namespace KingDomino.Tests
 {
-    class ViewModelTests
+    [TestClass()]
+    public class ViewModelTests
     {
+        [TestMethod()]
+        public void ViewModelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePlayersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisplayChatMessageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateChosenDominoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateChosenTileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdatePlacedTileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SwitchBoardViewTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetBoardTileVisiblityTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateScoresTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetCurrentDominosFromNextDominosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateBackFacingDominosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ShowOptionsTest()
+        {
+            Assert.Fail();
+        }
     }
 }
