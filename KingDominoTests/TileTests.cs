97fdeab04b80cefeaf26b1aa39eca7f5121ec1e2
bjm31 <bjm31@students.uwf.edu>
2018-04-25 11:15:13 -0500

    Tile test done

---
+++ KingDominoTests/TileTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingDomino;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDomino.Tests
+{
+    [TestClass()]
+    public class TileTests
+    {
+        [TestMethod()]
+        public void TileTest()
+        {
+            Tile placeholderTile = new Tile("Resources/Misc/logo.png", TileType.Null, 0);
+            Assert.IsNotNull(placeholderTile);
+            Assert.AreEqual(TileType.Null, placeholderTile.TileType);
+        }   
+    }
+}
